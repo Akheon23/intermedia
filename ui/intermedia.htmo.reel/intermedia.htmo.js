043b67f735fb9339e0d614ad0c886877f7531e2e
Christopher Werkshage <noreply>
2016-12-03 09:00:55 +0000

    Add component intermedia.htmo

---
+++ ui/intermedia.htmo.reel/intermedia.htmo.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/intermedia.htmo.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Intermedia.htmo
+ * @extends Component
+ */
+exports.Intermedia.htmo = Component.specialize(/** @lends Intermedia.htmo# */ {
+    constructor: {
+        value: function Intermedia.htmo() {
+            this.super();
+        }
+    }
+});
